5488b4af6bc113bc6ad6f06546edce43a47d0dcd
Michelle Poterek <poterekm@gmail.com>
2017-06-02 13:40:51 -0700

    add question number 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var add = function(question1, question2, question3, question4) {
-  return question1 + question2 + question3 + question4;
+var add = function(question1, question2, question3, question4, question5) {
+  return question1 + question2 + question3 + question4 + question5;
 };
 
 
@@ -21,17 +21,19 @@ $(document).ready(function(){
     console.log(question3);
     var question4 = parseInt($("input:radio[name=question4]:checked").val());
     console.log(question4);
-    var userValue = add(question1, question2, question3, question4);
+    var question5 = parseInt($("input:radio[name=question5]:checked").val());
+    var userValue = add(question1, question2, question3, question4, question5);
     console.log(userValue)
 
-    if (userValue <= 4) {
+    if (userValue <= 5) {
       $("#cSharp").show();
       $("#initialShow").hide();
-    } else if (userValue <=8) {
-      $("#initialPageShow").hide();
+    } else if (userValue <= 10) {
       $("#java").show();
-    } else if (userValue <= 12) {
+      $("#initialShow").hide();
+    } else if (userValue <= 15) {
       $("#ruby").show();
+      $("#initialShow").hide();
     }
     // console.log(result);
     $("#trackResult").show(userValue);
