12a2c6a02c9770368ff3b9cc894630f27a0a1147
Michelle Poterek <poterekm@gmail.com>
2017-06-02 11:53:34 -0700

    define add function in backend logic then define the variable uservalue and call to the add function

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
-var userValue = function(question1, question2, question3, question4) {
+var add = function(question1, question2, question3, question4) {
   return question1 + question2 + question3 + question4;
 };
+
+
+
 
 console.log("boop");
 
@@ -9,7 +12,7 @@ console.log("boop");
 $(document).ready(function(){
   $("#radioForm").submit(function(){
     event.preventDefault();
-    console.log("boop");
+    console.log("boop2");
     var question1 = parseInt($("input:radio[name=question1]:checked").val());
     console.log(question1);
     var question2 = parseInt($("input:radio[name=question2]:checked").val());
@@ -18,8 +21,9 @@ $(document).ready(function(){
     console.log(question3);
     var question4 = parseInt($("input:radio[name=question4]:checked").val());
     console.log(question4);
-    alert(userValue(question1, question2, question3, question4));
-    console.log(userValue);
+    var userValue = add(question1, question2, question3, question4);
+    // alert(userValue(question1, question2, question3, question4));
+    console.log(userValue)
 
     var result;
     if (userValue <= 4) {
