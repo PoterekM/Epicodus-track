2d077ce4e83d6381cdc21358dbb91b588593701f
Michelle Poterek <poterekm@gmail.com>
2017-06-02 15:40:20 -0700

    remove console logs

--- js/scripts.js
+++ js/scripts.js
@@ -5,25 +5,15 @@ var add = function(question1, question2, question3, question4, question5) {
 
 
 
-console.log("boop");
-
-
-
 $(document).ready(function(){
   $("#radioForm").submit(function(){
     event.preventDefault();
-    console.log("boop2");
     var question1 = parseInt($("input:radio[name=question1]:checked").val());
-    console.log(question1);
     var question2 = parseInt($("input:radio[name=question2]:checked").val());
-    console.log(question2);
     var question3 = parseInt($("input:radio[name=question3]:checked").val());
-    console.log(question3);
     var question4 = parseInt($("input:radio[name=question4]:checked").val());
-    console.log(question4);
     var question5 = parseInt($("input:radio[name=question5]:checked").val());
     var userValue = add(question1, question2, question3, question4, question5);
-    console.log(userValue)
 
     if (userValue <= 5) {
       $("#cSharp").show();
