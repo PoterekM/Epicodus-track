0d10881265d89a434b8b7489ca1b795542c4a1de
Michelle Poterek <poterekm@gmail.com>
2017-06-02 10:15:26 -0700

    add html div for the track result, hide it initially in css, and show it after the submit form is filled out

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var userValue = function(question1, question2) {
-  return question1 + question2;
+var userValue = function(question1, question2, question3, question4) {
+  return question1 + question2 + question3 + question4;
 };
 
 
@@ -16,9 +16,13 @@ $(document).ready(function(){
     console.log(question1);
     var question2 = parseInt($("input:radio[name=question2]:checked").val());
     console.log(question2);
-    // var question3 = parseInt($("input:radio[name=question3]:checked").val());
-    // console.log(question3);
-    alert(userValue(question1, question2));
+    var question3 = parseInt($("input:radio[name=question3]:checked").val());
+    console.log(question3);
+    var question4 = parseInt($("input:radio[name=question4]:checked").val());
+    console.log(question3);
+    alert(userValue(question1, question2, question3, question4));
+
+    $("#trackResult").show();
   });
 
 });
