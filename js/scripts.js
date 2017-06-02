fb89e8751d0882a2d2150dfdce89e3b43b9e5b35
Michelle Poterek <poterekm@gmail.com>
2017-06-02 09:28:43 -0700

    add Question 2 in the radio button segment as well as corresponding jQuery to get value attached to user input

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,10 @@ $(document).ready(function(){
   $("#radioForm").submit(function(){
     event.preventDefault();
     console.log("boop");
-    var question1 = $parseInt(("input:radio[name=question1]:checked").val());
+    var question1 = parseInt($("input:radio[name=question1]:checked").val());
     console.log(question1);
+    var question2 = parseInt($("input:radio[name=question2]:checked").val());
+    console.log(question2);
   });
 
 });
