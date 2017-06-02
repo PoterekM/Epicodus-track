ffcb3b9dee3c66572bb61b1d3ac296e1e5be42d5
Michelle Poterek <poterekm@gmail.com>
2017-06-02 09:08:27 -0700

    add parseInt in appropriate place to get user value to return as a number value

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $("#radioForm").submit(function(){
     event.preventDefault();
     console.log("boop");
-    var question1 = $("input:radio[name=question1]:checked").val();
+    var question1 = $parseInt(("input:radio[name=question1]:checked").val());
     console.log(question1);
   });
 
