da906a0f427f1fa964bb335a8bdb5a3fda306e8e
Michelle Poterek <poterekm@gmail.com>
2017-06-02 08:58:57 -0700

    ensure code for button, radio form, and value correlating to radio button is working after checking with console logs

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
-  $(#radioForm).submit(function(){
+  $("#radioForm").submit(function(){
+    event.preventDefault();
+    console.log("boop");
     var question1 = $("input:radio[name=question1]:checked").val();
-});
+    console.log(question1);
+  });
 
-})
+});
